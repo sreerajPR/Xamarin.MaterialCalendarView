eeafef4472d8591073c1d16f4aa00a063e8c832d
sreerajpr <sreeraj.pr@fingent.com>
2016-04-04 18:14:15 +0530

    Added Sample Demo Project

---
+++ MaterialCalendarSample/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace MaterialCalendarSample
+{
+	[Activity (Label = "MaterialCalendarSample", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			var materialCalendar= new MaterialCalendarLibrary.MaterialCalendarView(this);
+
+			SetContentView (materialCalendar);
+		}
+	}
+}
+
+
