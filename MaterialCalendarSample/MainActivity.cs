6b4fcf4b50561cfa9a1e604c43df5a0ea4ac338a
sreerajpr <sreeraj.pr@fingent.com>
2016-04-18 19:21:12 +0530

    Reverted MonthView Public

--- MaterialCalendarSample/MainActivity.cs
+++ MaterialCalendarSample/MainActivity.cs
@@ -1,15 +1,16 @@
 ﻿using Android.OS;
-using Android.App;
 using MaterialCalendarLibrary;
 using Android.Graphics.Drawables;
 using Android.Graphics;
+using Android.App;
+using System;
+using Android.Runtime;
 
 namespace MaterialCalendarSample
 {
 	[Activity (Label = "MaterialCalendarSample", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
 
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
@@ -19,24 +20,45 @@ namespace MaterialCalendarSample
 
 			var materialCalendar= FindViewById<MaterialCalendarView>(Resource.Id.calendarView);
 
-			materialCalendar.SelectionMode = MaterialCalendarView.SelectionModeNone;
+			materialCalendar.SelectionMode = MaterialCalendarView.SelectionModeSingle;
 
 			materialCalendar.AddDecorator (new CalendarDayViewDecorator ());
 		}
 	}
 	class CalendarDayViewDecorator:Java.Lang.Object,IDayViewDecorator
 	{
-		int pp=0;
+		
 		public bool ShouldDecorate (CalendarDay p0)
 		{
-			return ((++pp) % 2 == 0);
+			if (p0.Day == 5 || p0.Day == 14 || p0.Day == 25)
+				return true;
+			else
+				return false;
 		}
 
 		public void Decorate (DayViewFacade p0)
 		{
-			p0.SetBackgroundDrawable (new ColorDrawable (Color.LightYellow));
+			p0.SetBackgroundDrawable (new ColorDrawable (Color.Rgb(37,155,36)));
 		}
 	}
+	interface ICustomDayViewDecorator:IDayViewDecorator
+	{
+		
+	}
+//	class CustomCalendarPagerView: MonthView 
+//	{
+//		public CustomCalendarPagerView (IntPtr javaReference,JniHandleOwnership transfer):base(javaReference,transfer)
+//		{
+//			
+//		}
+//
+//		public CustomCalendarPagerView (MaterialCalendarView p0, CalendarDay p1,int p2):base(p0,p1,p2)
+//		{
+//
+//		}
+//
+//
+//	}
 }
 
 
